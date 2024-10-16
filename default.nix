7ba5d352b425dfd317d140fcfdc250732dd8d88e
Indy Ray <arelius@gmail.com>
2024-10-16 12:13:06 -0700

    Adding glove80 keymap and build config.

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   zmk = (import ../zmk) {};
-in {
+in rec {
   cradio_left = zmk.zmk.override {
     shield = "cradio_left";
     board = "nice_nano_v2";
@@ -14,4 +14,14 @@ in {
     keymap = ./config/cradio.keymap;
     kconfig = ./config/cradio.conf;
   };
+
+  glove80_left = zmk.glove80_left.override {
+    keymap = ./config/glove80.keymap;
+  };
+
+  glove80_right = zmk.glove80_right.override {
+    keymap = ./config/glove80.keymap;
+  };
+
+  glove80_combined = zmk.combine_uf2 glove80_left glove80_right;
 }
